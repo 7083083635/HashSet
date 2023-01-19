468fbdbc198d8a07b372328918bbb2e1bf42e5a3
Krishna <krishnarathod373@gmail.com>
2023-01-19 22:36:27 +0530

    done

---
+++ src/com/velocity/Demo.java
@@ -0,0 +1,20 @@
+package com.velocity;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Demo {
+	
+	public static void main(String[] args) {
+		
+		Set<Integer> demo = new HashSet<Integer>();
+		demo.add(2);
+		demo.add(4);
+		demo.add(3);
+		demo.add(1);
+		
+		System.out.println(demo);
+		
+	}
+
+}
---
+++ src/com/velocity/HashMap.java
@@ -0,0 +1,12 @@
+package com.velocity;
+
+import java.util.Map;
+
+public class HashMap {
+	
+	public static void main(String[] args) {
+	
+		System.out.println("Krishna");
+	}
+
+}
